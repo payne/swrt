82507875f3b6acc2c7d8a7152f2c44212a30db8c
Matt Payne <Payne@MattPayne.org>
2022-05-09 16:41:32 -0500

    console logs

--- swrt.js
+++ swrt.js
@@ -1,6 +1,7 @@
 // Helpful documenation
 // https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/JSON#active_learning_working_through_a_json_example
 
+console.log("About to start!!!");
 const request = new Request('ring.json');
 const response = await fetch(request);
 console.log(response);
