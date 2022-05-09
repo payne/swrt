2c3cd787a968110d4b366dec7cf665b0b292d31b
Matt Payne <Payne@MattPayne.org>
2022-05-09 16:50:53 -0500

    Doing HTTP GET with basic javascript

--- swrt.js
+++ swrt.js
@@ -2,7 +2,15 @@
 // https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/JSON#active_learning_working_through_a_json_example
 
 console.log("About to start!!!");
-const request = new Request('ring.json');
-const response = await fetch(request);
-console.log(response);
+// alert("Hello!");
+/* */
+
+async function getData() {
+  const request = new Request('ring.json');
+  const response = await fetch(request);
+  console.log(response);
+}
+
+console.log("calling getData()");
+getData();
 
